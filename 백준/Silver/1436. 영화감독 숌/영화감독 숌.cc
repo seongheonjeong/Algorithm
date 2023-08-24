fb776a2919ab61a3fa24b587f35bc096a574c4f0
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-08-24 20:57:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 72 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    vector<int> v;
+    int cnt = 0;
+    cin >> N;
+    for (int i = 0; ; i++) // 1부터 천만까지 범위 수정
+    {
+        if (to_string(i).find("666") != string::npos)
+        {
+            v.push_back(i);
+            cnt++;
+        }
+        if (cnt == 10000)
+            break;
+    }
+    cout << v[N - 1]<<'\n';
+}
