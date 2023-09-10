e57808882d5b9f55e0541fc8422dcfffd441a29e
정성헌 <121993854+seongheonjeong@users.noreply.github.com>
2023-09-10 22:57:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cin >> num;
+	for (int i = 1; i <= num; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
