97aec89b28c38d0f6db434048858b45be8265db7
정성헌 <121993854+seongheonjeong@users.noreply.github.com>
2023-09-11 21:53:22 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,12 +7,13 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
 		
 		System.out.println(a+b);
+
 	}
+
 }
