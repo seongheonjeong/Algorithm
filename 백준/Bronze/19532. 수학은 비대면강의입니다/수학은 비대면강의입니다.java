4547f64290eee2fd4bb87ccb131ae8c593794a7c
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-09-18 18:16:35 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 204 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    
+    int answer=0,answer1=0;
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int d=sc.nextInt();
+    int e=sc.nextInt();
+    int f=sc.nextInt();
+    
+    answer=(e*c-b*f)/(a*e-b*d);
+    answer1=(d*c-a*f)/(b*d-a*e);
+    
+    System.out.print(answer);
+    System.out.print(' ');
+    System.out.print(answer1);
+    
+    
+    
+    
+    
+    
+//    long answer=1;
+//    if(N==0)
+//    {
+//    	System.out.println(1);
+//    	return;
+//    }
+//    for(int i=1; i<=N; i++)
+//    {
+//    	answer*=i;
+//    }
+//    System.out.println(answer);
+//    int a,b,c,d,e,f;
+//    int ans1=0,ans2=0; //x,y
+//    a=sc.nextInt();
+//    b=sc.nextInt();
+//    c=sc.nextInt();
+//    d=sc.nextInt();
+//    e=sc.nextInt();
+//    f=sc.nextInt();
+//    
+//    a*=d; b*=d; c*=d; //x+3y=-1
+//    d*=a; e*=a; f*=a; //4x+y=7   4x+12y=14 
+////    ans1=(b+e)/(c+f);
+//    if(a+d==0)
+//    {
+//    	
+//    }
+    
+    //    System.out.println("|\\_/|");
+//    System.out.println("|q p|   /}");
+//    System.out.println("( 0 )\"\"\"\\");
+//    System.out.println("|\"^\"`    |");
+//    System.out.println("||_/=\\\\__|");
+//    
+//    int N,answer=1;
+//    N=sc.nextInt();
+//    if(N==0)
+//    {
+//    	System.out.println(1);
+//    	return;
+//    }
+//    for(int i=1; i<=N; i++)
+//    {
+//      answer*=i;
+//    }
+//    System.out.println(answer);
+//  }
+ }
+}
