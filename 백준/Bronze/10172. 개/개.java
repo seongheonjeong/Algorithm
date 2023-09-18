baec5b04de2be57df6341d7176d6fba2312f24ee
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-09-18 17:28:38 +0900

    [Bronze V] Title: 개, Time: 192 ms, Memory: 17332 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+    
+//    int N,answer=1;
+//    N=sc.nextInt();
+//    if(N==0)
+//    {
+//    	System.out.println(1);
+//    	return;
+//    }
+//    for(int i=1; i<=N; i++)
+//    {
+//      answer*=i;
+//    }
+//    System.out.println(answer);
+//  }
+ }
+}
