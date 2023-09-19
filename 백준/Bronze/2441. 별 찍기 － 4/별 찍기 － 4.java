421806b0225dc9578f21f7dbd31964f25f1f5b1b
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-09-19 17:34:26 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 400 ms, Memory: 23336 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		for(int i=0; i<N; i++)
+		{
+			for(int x=0; x<i; x++)
+			{
+				System.out.print(' ');
+			}
+			for(int j=i; j<N; j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
