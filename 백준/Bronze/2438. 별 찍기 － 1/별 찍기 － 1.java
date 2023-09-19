0767465a0fac5c7d1840b2f2e6b91273c596cca2
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-09-19 17:18:44 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 312 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		for(int i=0; i<N; i++)
+		{
+			for(int j=0; j<i+1; j++)
+			{
+				System.out.print("*");
+			
+			}
+			System.out.println();
+		}
+	}
+
+}
