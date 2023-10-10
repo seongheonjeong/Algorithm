faf7c3b441aa9fb2c9c0608500e4c9e80a936e4a
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-10-10 16:25:22 +0900

    [Bronze V] Title: Hello World, Time: 124 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Stack;
+public class Main {
+//    static String[]chess=new String[51];
+//    static String[]whiteChess={"WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW"};
+//    static String[]blackChess={"BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB"};
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+
+        bw.flush();
+        bw.close();
+    }
+//
+//    public int cnt()
+//    {
+//
+//    }
+}
