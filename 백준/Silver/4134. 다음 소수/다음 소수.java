66c16a598c5b6ca0bc040f9450a52f6fb19c99e1
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-10-12 13:41:18 +0900

    [Silver IV] Title: 다음 소수, Time: 444 ms, Memory: 15328 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static boolean prime(long a)
+    {
+        if(a<2)
+            return false;
+        for(long i=2; i*i<=a; i++)
+        {
+            if(a%i==0)
+                return false;
+        }
+
+            return true;
+    }
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T=Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++)
+        {
+            Long n=Long.parseLong(br.readLine());
+            while(true)
+            {
+                if(prime(n))
+                {
+                    System.out.println(n);
+                    break;
+                }
+                else
+                    n++;
+            }
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+
+}
