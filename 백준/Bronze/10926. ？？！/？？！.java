b671e2ff3bb8bdb38802f3e20ae575a1ce2c6412
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2023-10-12 14:23:42 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        bw.write(br.readLine()+"??!"+"\n");
+        bw.flush();
+        bw.close();
+    }
+
+}
