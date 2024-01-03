50afb399cbc9339dae60a74b36d3eb84e4f1f85e
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2024-01-03 13:55:42 +0900

    [Silver IV] Title: 문자열 집합, Time: 84 ms, Memory: 7952 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M,answer;
+map<string, int>m;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		string s;
+		cin >> s;
+		m.insert({ s,1 });
+	}
+	for (int i = 0; i < M; i++)
+	{
+		string temp;
+		cin >> temp;
+		if (m.find(temp) != m.end())
+			answer++;
+	}
+	cout << answer;	
+}
