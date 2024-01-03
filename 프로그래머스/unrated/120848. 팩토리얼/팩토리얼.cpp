363034976f44d630205d5508d8e8e8a25d067694
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2024-01-03 21:31:01 +0900

    [level unrated] Title: 팩토리얼, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+
+int solution(int n) {
+    int i = 1;
+    int answer = 1;
+
+    while (answer <= n) {
+        i++;
+        answer *= i;
+    }
+
+    return i - 1;
+}
