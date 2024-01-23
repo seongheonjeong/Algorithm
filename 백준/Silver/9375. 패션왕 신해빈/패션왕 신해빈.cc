f1f3ed22afbffc6d690895f0e2ba22ca60107dcc
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2024-01-24 00:53:21 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,T,answer=1;
+map<string, int>m;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> n;
+        m.clear();
+        for (int j = 0; j < n; j++)
+        {
+            string s1, s2;
+            cin >> s1 >> s2;
+            m[s2]++;      
+        }
+
+        for (auto iter = m.begin(); iter != m.end(); iter++)
+        {
+            answer *= (iter->second )+1;
+        }
+        cout << answer-1 << '\n';
+        answer = 1;
+    }
+
+
+}
