84f4e8f7a42b5dc5a76bf4331bcdd07eddfe5e0a
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2024-01-29 08:54:36 +0900

    [Gold V] Title: 백도어, Time: 304 ms, Memory: 28068 KB -BaekjoonHub

---
+++ 백준/Gold/17396. 백도어/백도어.cc
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define INF 1234987654321
+using namespace std;
+
+int N, M,a,b,t;
+long long dist[100001];
+long long v[100001]; //갈 수 있는 곳인지 아닌지(시야에 보이는가)
+vector<pair<long, long>>graph[100001];
+priority_queue<pair<int, long long>, vector<pair<int, long long>>, greater<pair<int, long long>>>pq;
+void dijkstra()
+{
+    fill(dist, dist + 100001, INF);
+    pq.push({ 0,0 });
+    dist[0] = 0;
+    while (!pq.empty())
+    {
+        pair<int, long long>cur = pq.top(); 
+        pq.pop();
+        
+        if (cur.second > dist[cur.first]) continue;
+        for (int i = 0; i < graph[cur.first].size(); i++)
+        {
+            pair<int, long long>next = graph[cur.first][i];
+            if (v[next.first] == 1) continue;
+            if (dist[cur.first]+next.second < dist[next.first])
+            {
+                dist[next.first] = next.second+dist[cur.first];
+                pq.push({ next.first,dist[next.first]});         
+            }
+        }
+    }
+}
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+    }
+    v[N - 1] = 0;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> a >> b >> t;
+        graph[a].push_back({b,t});
+        graph[b].push_back({ a,t });
+    }
+    dijkstra();
+    if (dist[N - 1] == INF)
+        cout << -1;
+    else
+        cout << dist[N - 1];
+}
