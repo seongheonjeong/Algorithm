5910bc99e8e54c77b2fe75ce05644344fcb34453
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2024-01-31 15:25:23 +0900

    [Gold V] Title: 백도어, Time: 144 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.cc
+++ 백준/Gold/17396. 백도어/백도어.cc
@@ -2,11 +2,11 @@
 #define INF 9999999999
 using namespace std;
 
-int N, M,a,b,t;
-long long dist[100001];
-int v[100001]; //갈 수 있는 곳인지 아닌지(시야에 보이는가)
-vector<pair<int,int>>graph[100001];
-priority_queue<pair<int, long long>, vector<pair<int, long long>>, greater<pair<int, long long>>>pq;
+int N, M, a, b, t; //노드,간선,출발점,도착점,가중치
+long long dist[100001]; //최단거리
+int v[100001]; //시야에 보이는지
+vector<pair<int, int>>graph[100001]; 
+priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>>pq;
 void dijkstra()
 {
     fill(dist, dist + 100001, INF);
@@ -14,23 +14,23 @@ void dijkstra()
     dist[0] = 0;
     while (!pq.empty())
     {
-        pair<int, long long>cur = pq.top(); 
+        pair<long long, int>cur = pq.top();
         pq.pop();
-        if (cur.first == N - 1)break;
-        if (cur.second > dist[cur.first]) continue;
-        for (int i = 0; i < graph[cur.first].size(); i++)
+        if (cur.second == N - 1)break;
+        if (cur.first > dist[cur.second]) continue;
+        for (int i = 0; i < graph[cur.second].size(); i++)
         {
-            pair<int, long long>next = graph[cur.first][i];
-            if (v[next.first] == 1) continue;
-            if (dist[cur.first]+next.second < dist[next.first])
+            pair<int,int>next = graph[cur.second][i]; //가중치,노드
+            if (v[next.second] == 1) continue;
+            if (dist[cur.second] + next.first < dist[next.second])
             {
-                dist[next.first] = next.second+dist[cur.first];          
-                pq.push({ next.first,dist[next.first]});         
+                dist[next.second] = dist[cur.second] + next.first;
+                pq.push({ dist[next.second], next.second });
             }
         }
     }
 }
-int main() 
+int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -43,8 +43,8 @@ int main()
     for (int i = 0; i < M; i++)
     {
         cin >> a >> b >> t;
-        graph[a].push_back({b,t});
-        graph[b].push_back({ a,t });
+        graph[a].push_back({ t,b });
+        graph[b].push_back({ t,a });
     }
     dijkstra();
     if (dist[N - 1] == INF)
