0482c9fe9aa65660ba6a14ad516a7c1342befe9e
seongheonjeong <121993854+seongheonjeong@users.noreply.github.com>
2025-09-01 22:51:37 +0900

    [D2] Title: 조교의 성적 매기기, Time: 5 ms, Memory: 408 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.cpp
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.cpp
@@ -9,7 +9,6 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	int T = 0,temp=1;
-double targetScore = 0;
 cin >> T;
 string grade[10] = { "A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0" };
 while (T--) 
@@ -20,11 +19,10 @@ while (T--)
 	for (int i = 1; i <= N; i++) {
 		double a = 0, b = 0, c = 0;
 		cin >> a>>b>>c;
-		scores.push_back(make_pair(i, a * 0.35 + b * 0.45 + c * 0.2));
-		if (K == i) targetScore = a * 0.35 + b * 0.45 + c * 0.2;
+		scores.push_back(make_pair(i, a * 0.35 + b * 0.45 + c * 0.2));		
 	}
 	sort(scores.begin(), scores.end(), [](auto& a, auto& b) {
-		return a.second > b.second;  // 점수 큰 게 앞으로
+		return a.second > b.second;  //내림차순
 		});
 	for (int i = 0; i < N; i++) {
 		if (scores[i].first == K) {
@@ -34,6 +32,7 @@ while (T--)
 	}
 	int index = rank / (N / 10);
 	cout << "#" << temp++ << " " << grade[index] << "\n";
-	}
-    return 0;
+}
+
+return 0;
 }
